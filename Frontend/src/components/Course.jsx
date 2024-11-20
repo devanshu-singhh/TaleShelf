053481986a20af0c2b970fb50914fa55b3c09ab3
devanshusingh76 <rajputdevanshu9084@gmail.com>
2024-11-20 21:38:37 +0530

    newChange

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -22,7 +22,7 @@ function Course() {
          <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col'>
             <div className='mt-28 items-center justify-center text-center'>
                <h1 className='text-2xl font-semibold md:text-4xl'>
-                  We're delighted to have you <span className='text-pink-500' >Here! :)</span>
+                  We're delighted to have you <span className='text-pink-500'> Here! :)</span>
                   </h1>
                   <p className='mt-12'>
                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur odit ut quaerat reiciendis nam dicta facilis? Non dicta odit praesentium! Facilis nostrum unde provident sit sequi totam maiores earum molestiae!
